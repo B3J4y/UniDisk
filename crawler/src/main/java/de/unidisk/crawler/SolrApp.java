126d6eb5210bae6464f7619fb699c9b683dfac30
Tim Sauvageot <tim.sauvageot@web.de>
2020-02-23 12:16:43 +0100

    Verwendung von Services

--- crawler/src/main/java/de/unidisk/crawler/SolrApp.java
+++ crawler/src/main/java/de/unidisk/crawler/SolrApp.java
@@ -2,16 +2,29 @@ package de.unidisk.crawler;
 
 import de.unidisk.common.SystemProperties;
 import de.unidisk.crawler.analysis.CrawlerDataAnalysis;
+import de.unidisk.crawler.contracts.IScoringService;
 import de.unidisk.crawler.datatype.Model;
 import de.unidisk.crawler.io.ReadMysql;
+import de.unidisk.crawler.model.ScoreResult;
 import de.unidisk.crawler.mysql.MysqlConnector;
+import de.unidisk.crawler.services.SolrScoringService;
 import de.unidisk.crawler.solr.SolrConnector;
 import de.unidisk.crawler.solr.SolrStandardConfigurator;
+import de.unidisk.dao.ProjectDAO;
+import de.unidisk.entities.hibernate.Keyword;
+import de.unidisk.entities.hibernate.Project;
+import de.unidisk.entities.hibernate.ProjectState;
+import de.unidisk.entities.hibernate.Topic;
+import de.unidisk.repositories.contracts.IProjectRepository;
+import de.unidisk.repositories.contracts.IResultService;
+import de.unidisk.services.HibernateResultService;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import java.io.FileInputStream;
 import java.util.Collection;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -19,80 +32,79 @@ import java.util.Properties;
  */
 public class SolrApp {
     static private final Logger logger = LogManager.getLogger(SolrApp.class.getName());
-    private static Properties systemProperties = SystemProperties.getInstance();
-    private String database;
+    private IProjectRepository projectRepository;
+    private IScoringService scoringService;
+    private IResultService resultService;
 
-    public SolrApp(String database) {
-        this.database = database;
+    public SolrApp(IProjectRepository projectRepository, IScoringService scoringService,
+                   IResultService resultService) {
+        this.projectRepository = projectRepository;
+        this.scoringService = scoringService;
+        this.resultService = resultService;
     }
 
-    public  void execute() throws Exception {
-        MysqlConnector mc = new MysqlConnector();
-        if (mc.checkCampaignStatus(this.database) == 1) {
-            logger.warn("Campaign is already computing");
+    private String projectLockFileName(Project p){
+        return p.getId()+"lock.unidisk";
+    }
+
+    public void execute() throws Exception {
+
+        final List<Project> pendingProjects = projectRepository.getProjects(ProjectState.WAITING);
+
+        if(pendingProjects.isEmpty()){
+            logger.info("No projects waiting for processing.");
             return;
         }
-        System.out.print("starting csv creation");
-        mc.setCampaignStatus(this.database, 1);
 
-        logger.debug("Entering main");
-        logger.info("Read out csv or mysql");
-        try {
-            //ReadCsv csv = new ReadCsv(MagicStrings.dataPath);
-            ReadMysql mysql = new ReadMysql();
-            SolrConnector connector = new SolrConnector(SolrStandardConfigurator.getStandardUrl(),
-                    systemProperties.getProperty("solr.connection.db"));
+        for(Project p : pendingProjects) {
+            final String lockFile = projectLockFileName(p);
 
-            CrawlerDataAnalysis cda = new CrawlerDataAnalysis(
-                    Integer.valueOf(systemProperties.getProperty("values.percentile.min")),
-                    Integer.valueOf(systemProperties.getProperty("values.percentile.max")), this.database);
-
-
-            Model model = mysql.convertToModel(database);
-            logger.info("New Model instance. Length - StichwortVar:" + model.stichwortVarSize() + " VarMeta:"
-                    + model.varMetaSize());
-            //model.insertSynonyms();
-            logger.info("Model instance with Synonyms. Length - StichwortVar:" + model.stichwortVarSize() + " VarMeta:"
-                    + model.varMetaSize());
-            logger.info("The model has been put into Neo4J");
-            logger.info("Create Query");
-            model.initStichFile("NOPATH");
-            model.initVarMetaFile("NOPATH");
-            model.scoreStichwort(connector, "NOPATH");
-            model.scoreVariable(connector, "NOPATH");
-            logger.info("Get Score from Crawling");
-            logger.info("Transform Scoring into results");
-            //model.stichwortVarToCsv(MagicStrings.stichWortVarPath);
-            //model.stichwortResultToCsv(MagicStrings.stichWortPath);
-            //model.varMetaResultToCsv(MagicStrings.varMetaPath);
-
-            for (String var : model.getVarMeta().getVariables()) {
-                logger.debug("Search percentile for " + var);
-                cda.prepareHochschuleSolrAnalyse(var);
-
-                double[] values = ArrayUtils.toPrimitive(cda.inputData.keySet().toArray(new Double[0]));
-
-                logger.debug("There are " + values.length + " elements in the table");
-                if (values.length > Integer.valueOf(systemProperties.getProperty("values.percentile.max"))) {
-                    Collection<String> relevantData = cda.selectRelevantDataForPlotting();
-                    cda.deleteInDatabase(relevantData, var);
-                } else {
-                    logger.debug("The results in the table don't exceed the maximum limit.");
-                }
+            FileInputStream in = new FileInputStream(lockFile);
+            java.nio.channels.FileLock lock = null;
+            try {
+                lock = in.getChannel().lock();
+            }catch(Exception e){
+                logger.error("Unable to lock project file.");
+                logger.error(e);
+                continue;
             }
-            mc.setCampaignStatus(this.database, 2);
-            logger.debug("Leaving main");
-        } catch (Exception e) {
-            logger.error(e);
 
-            mc.setCampaignStatus(this.database, 3);
-            e.getStackTrace();
+            logger.info("Start processing project: " + p.getName());
+            projectRepository.updateProjectState(p.getId(), ProjectState.RUNNING);
 
+            logger.debug("Entering main");
+            logger.info("Read out csv or mysql");
+            try {
+
+                final List<Topic> topics = p.getTopics();
+                for(Topic t : topics){
+
+                    for(Keyword keyword : t.getKeywords()){
+                        final ScoreResult score = this.scoringService.getKeywordScore(p.getId(),keyword.getId());
+                        this.resultService.CreateKeywordScore(score);
+                    }
+                    final ScoreResult topicScore = this.scoringService.getTopicScore(p.getId(), t.getId());
+                    this.resultService.CreateTopicScore(topicScore);
+                }
+                projectRepository.updateProjectState(p.getId(),ProjectState.FINISHED);
+            } catch (Exception e) {
+                logger.error(e);
+
+                projectRepository.updateProjectState(p.getId(),ProjectState.ERROR);
+                e.getStackTrace();
+
+            }finally{
+                lock.close();
+            }
         }
 
     }
     public static void main(String[] args) throws Exception {
-        SolrApp sapp = new SolrApp("up_test");
+        final IScoringService scoringService = new SolrScoringService();
+        final IProjectRepository projectRepository = new ProjectDAO();
+        final IResultService resultService = new HibernateResultService();
+
+        SolrApp sapp = new SolrApp(projectRepository,scoringService,resultService);
         sapp.execute();
     }
 }
