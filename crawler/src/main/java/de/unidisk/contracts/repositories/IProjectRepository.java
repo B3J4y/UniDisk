a199adbf588b0e7941ec621067df56017bcec133
timsauvageot <tim.sauvageot@web.de>
2021-03-23 12:01:59 +0100

    implement

--- crawler/src/main/java/de/unidisk/contracts/repositories/IProjectRepository.java
+++ crawler/src/main/java/de/unidisk/contracts/repositories/IProjectRepository.java
@@ -1,10 +1,12 @@
 package de.unidisk.contracts.repositories;
 
+import de.unidisk.common.exceptions.EntityNotFoundException;
 import de.unidisk.contracts.exceptions.DuplicateException;
 import de.unidisk.contracts.repositories.params.project.CreateProjectParams;
 import de.unidisk.contracts.repositories.params.project.UpdateProjectParams;
 import de.unidisk.entities.hibernate.Project;
 import de.unidisk.entities.hibernate.ProjectState;
+import de.unidisk.entities.hibernate.ResultRelevance;
 import de.unidisk.view.project.ProjectView;
 import de.unidisk.view.results.Result;
 
@@ -41,4 +43,6 @@ public interface IProjectRepository extends Serializable {
     void updateProjectState(int projectId, ProjectState state);
     void setProjectError(int projectId, String message);
     void clearProjectError(int projectId);
+
+    void rateTopicScore(String topicScoreId, ResultRelevance relevance) throws EntityNotFoundException;
 }
---
+++ crawler/src/main/java/de/unidisk/rest/ProjectResult.java
@@ -0,0 +1,44 @@
+package de.unidisk.rest;
+
+import de.unidisk.common.exceptions.EntityNotFoundException;
+import de.unidisk.contracts.repositories.IProjectRepository;
+import de.unidisk.rest.authentication.AuthNeeded;
+import de.unidisk.rest.authentication.ContextUser;
+import de.unidisk.rest.dto.project.RateResultDto;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@Path("/result")
+public class ProjectResult {
+
+    @Inject
+    IProjectRepository projectRepository;
+
+    public ProjectResult() {
+    }
+
+    public ProjectResult(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @POST
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @AuthNeeded
+    public Response rate(RateResultDto dto, @PathParam("id") String id, @Context SecurityContext context){
+        final ContextUser user = (ContextUser) context.getUserPrincipal();
+        try {
+            projectRepository.rateTopicScore(id,dto.getRelevance());
+        } catch (EntityNotFoundException e) {
+            return Response.status(404).build();
+        }
+        return Response.ok().build();
+    }
+
+}
