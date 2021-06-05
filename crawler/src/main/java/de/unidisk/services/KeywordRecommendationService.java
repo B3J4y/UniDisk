da02f628e2c05bccba555eba4ea8afbce982fca9
timsauvageot <tim.sauvageot@web.de>
2021-06-05 11:03:31 +0200

    fix recommendation http request

--- crawler/src/main/java/de/unidisk/services/KeywordRecommendationService.java
+++ crawler/src/main/java/de/unidisk/services/KeywordRecommendationService.java
@@ -9,10 +9,7 @@ import javax.ws.rs.ext.Provider;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -49,7 +46,8 @@ public class KeywordRecommendationService implements IKeywordRecommendationServi
     @Override
     public RecommendationResult getTopicRecommendations(String topic) {
         try {
-            URL url = new URL("http://localhost:8083/similiar?q="+topic);
+            String queryParam = "q="+URLEncoder.encode(topic,"UTF-8");
+            URL url =  new URL("http://localhost:8083/similiar?"+queryParam);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestProperty("Content-Type", "application/json");
             con.setRequestMethod("GET");
@@ -59,7 +57,7 @@ public class KeywordRecommendationService implements IKeywordRecommendationServi
                 return null;
 
             BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
+                    new InputStreamReader(con.getInputStream(),"UTF-8"));
             String inputLine;
             StringBuffer content = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
