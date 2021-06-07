2fe1e7561cde83c8909a4adb9eedf2f5cd57dc46
timsauvageot <tim.sauvageot@web.de>
2021-06-07 10:47:56 +0200

    http helper

--- crawler/src/main/java/de/unidisk/services/KeywordRecommendationService.java
+++ crawler/src/main/java/de/unidisk/services/KeywordRecommendationService.java
@@ -1,15 +1,16 @@
 package de.unidisk.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.unidisk.common.HttpHelper;
 import de.unidisk.contracts.services.recommendation.IKeywordRecommendationService;
 import de.unidisk.contracts.services.recommendation.KeywordRecommendation;
 import de.unidisk.contracts.services.recommendation.RecommendationResult;
 
 import javax.ws.rs.ext.Provider;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.*;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -47,26 +48,13 @@ public class KeywordRecommendationService implements IKeywordRecommendationServi
     public RecommendationResult getTopicRecommendations(String topic) {
         try {
             String queryParam = "q="+URLEncoder.encode(topic,"UTF-8");
-            URL url =  new URL("http://localhost:8083/similiar?"+queryParam);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setRequestMethod("GET");
-
-            int status = con.getResponseCode();
-            if(status != 200)
+            String url ="http://localhost:8083/similiar?"+queryParam;
+            HttpHelper.HttpResponse response = HttpHelper.get(url);
+            if(response.getStatusCode() != 200)
                 return null;
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream(),"UTF-8"));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
-            con.disconnect();
             ObjectMapper mapper = new ObjectMapper();
-            ResponseDto dto = mapper.readValue(content.toString(),ResponseDto.class);
+            ResponseDto dto = mapper.readValue(response.getBody(),ResponseDto.class);
             List<KeywordRecommendation> recommendations =    Arrays.stream(dto.getSimiliar()).map(pair -> {
                 ArrayList<Object> values =  (ArrayList<Object>) pair;
                 double score = (double)values.get(1);
