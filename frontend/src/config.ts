a05feb75f704c95e24cd2445637d7ded23814bdc
Tim Sauvageot UP <tim.sauvageot@web.de>
2021-07-13 17:44:28 +0000

    deploy

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -7,13 +7,13 @@ export const THEME = { primary: '#2b669a', colorOnPrimary: 'white' };
 // Determines whether stubs or API endpoints are used.
 export const USE_STUBS = false;
 
-const LOCAL_DOCKER_ENDPOINT = 'http://stud-01.cs.uni-potsdam.de:80/unidisk/rest/';
+const LOCAL_DOCKER_ENDPOINT = 'http://stud-01.cs.uni-potsdam.de:80/api/unidisk/rest/';
 //IntelliJ
 const LOCAL_DEV_ENDPOINT = 'http://localhost:8080/unidisk_war/rest/';
 
 export const API_ENDPOINT = process.env.API_ENDPOINT ?? LOCAL_DOCKER_ENDPOINT;
 
 export const KEYWORD_SERVICE_ENDPOINT =
-  process.env.RECOMMENDATION_ENDPOINT ?? 'http://localhost:8083';
+  process.env.RECOMMENDATION_ENDPOINT ?? 'http://stud-01.cs.uni-potsdam.de:80/recommendation';
 
 export const USE_FIREBASE_AUTH = true;
