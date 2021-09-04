314c7966ac190e2595aa750f26092f11a9021aee
timsauvageot <tim.sauvageot@web.de>
2021-09-04 11:45:06 +0200

    update crawl doc handling

--- crawler/src/main/java/de/unidisk/crawler/model/CrawlDocument.java
+++ crawler/src/main/java/de/unidisk/crawler/model/CrawlDocument.java
@@ -39,16 +39,19 @@ public class CrawlDocument {
 
     public CrawlDocument(SolrDocument document){
         this.id = (String) document.get("id");
-        this.title =  getProperty("title", document);
-        this.content = getProperty("content",document);
-        this.datum = getProperty("datum",document);
-        this.url = getProperty("url",document);
-        Long d = getProperty("depth",document);
+        this.title =  getProperty("title", document,"");
+        this.content = getProperty("content",document,"");
+        this.datum = getProperty("datum",document,0l);
+        this.url = getProperty("url",document,"http://www.google.com");
+        Long d = getProperty("depth",document,0l);
         this.depth = d.intValue();
     }
 
-    private static <T> T getProperty(String field, SolrDocument document){
-        return ((ArrayList<T>) document.get(field)).get(0);
+    private static <T> T getProperty(String field, SolrDocument document, T defaultValue){
+        final Object fieldObject =  document.get(field);
+        if(fieldObject == null)
+            return defaultValue;
+        return ((ArrayList<T>) fieldObject).get(0);
     }
     public static String[] getFields(){
         return new String[]{
