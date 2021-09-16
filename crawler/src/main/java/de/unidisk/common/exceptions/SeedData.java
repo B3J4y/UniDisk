aeb0544eb8ec6dea9cfd7ee0783eeb92831da4c4
timsauvageot <tim.sauvageot@web.de>
2021-09-16 13:54:58 +0200

    save

--- crawler/src/main/java/de/unidisk/common/exceptions/SeedData.java
+++ crawler/src/main/java/de/unidisk/common/exceptions/SeedData.java
@@ -10,17 +10,10 @@ public class SeedData {
 
     public static List<University> getSeedUniversities(){
         return Arrays.asList(
-                new University("Universität Potsdam",52.4010314,13.0097211,"https://www.uni-potsdam.de"),
-                new University("TU Berlin",52.5125322,13.3247559,"https://www.tu-berlin.de"),
-                new University("FH Potsdam", 52.4132004,13.0483612,"https://www.fh-potsdam.de"),
-                new University("HPI", 52.4132004,13.0097211,"https://hpi.de/"),
-                new University("TU München",48.1486926,11.5686501,"https://www.tum.de/"),
-                new University("FH Aachen",50.7647135,6.0792746,"https://www.fh-aachen.de"),
-                new University("Fernuni Hagen",51.3776306,7.4923537,"https://www.fernuni-hagen.de"),
-                new University("Universität Köln",50.9281865,6.928763,"https://www.uni-koeln.de"),
-                new University("LMU München",48.1510078,11.5798872,"https://www.lmu.de"),
-                new University("RWTH Aachen",50.7776697,6.0779794,"https://www.rwth-aachen.de"),
-                new University("Johann Wolfgang Göthe Universität Frankfurt",50.1270675,8.6655748,"https://www.uni-frankfurt.de")
-        );
+                new University("Uni Tübingen",52.4010314,13.0097211,"https://www.uni-tuebingen.de"),
+                new University("Uni Potsdam",52.4010314,13.0097211,"https://publishup.uni-potsdam.de"),
+                new University("TU München",48.1486926,11.5686501,"https://www.tum.de/")
+
+      );
     }
 }
--- crawler/src/main/java/de/unidisk/crawler/model/CrawlDocument.java
+++ crawler/src/main/java/de/unidisk/crawler/model/CrawlDocument.java
@@ -11,8 +11,6 @@ public class CrawlDocument {
 
     @Field
     public String url;
-    @Field
-    public String name;
 
     @Field
     public String title;
@@ -41,14 +39,22 @@ public class CrawlDocument {
         this.id = (String) document.get("id");
         this.title =  getProperty("title", document);
         this.content = getProperty("content",document);
-        this.datum = getProperty("datum",document);
+        this.datum = getProperty("datum",document,0l);
         this.url = getProperty("url",document);
-        Long d = getProperty("depth",document);
+        Long d = getProperty("depth",document,0l);
         this.depth = d.intValue();
     }
-
     private static <T> T getProperty(String field, SolrDocument document){
-        return ((ArrayList<T>) document.get(field)).get(0);
+        final Object fieldProp = document.get(field);
+        return ((ArrayList<T>) fieldProp).get(0);
+    }
+
+    private static <T> T getProperty(String field, SolrDocument document, T fallback){
+        final Object fieldProp = document.get(field);
+        if(fieldProp == null){
+            return fallback;
+        }
+        return ((ArrayList<T>) fieldProp).get(0);
     }
     public static String[] getFields(){
         return new String[]{
