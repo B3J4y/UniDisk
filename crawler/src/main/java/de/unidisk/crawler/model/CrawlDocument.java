16e1a4dd5a34a43b9aea96f703c14be08d4dd2d9
Tim Sauvageot UP <tim.sauvageot@web.de>
2021-09-25 14:38:09 +0000

    handle unavailable title

--- crawler/src/main/java/de/unidisk/crawler/model/CrawlDocument.java
+++ crawler/src/main/java/de/unidisk/crawler/model/CrawlDocument.java
@@ -39,17 +39,27 @@ public class CrawlDocument {
 
     public CrawlDocument(SolrDocument document){
         this.id = (String) document.get("id");
-        this.title =  getProperty("title", document);
+        this.title =  getProperty("title", document,"");
         this.content = getProperty("content",document);
-        this.datum = getProperty("datum",document);
+        this.datum = getProperty("datum",document,0l);
         this.url = getProperty("url",document);
-        Long d = getProperty("depth",document);
+        Long d = getProperty("depth",document,0l);
         this.depth = d.intValue();
     }
 
     private static <T> T getProperty(String field, SolrDocument document){
-        return ((ArrayList<T>) document.get(field)).get(0);
+        final Object fieldProp = document.get(field);
+        return ((ArrayList<T>) fieldProp).get(0);
     }
+
+    private static <T> T getProperty(String field, SolrDocument document, T fallback){
+        final Object fieldProp = document.get(field);
+        if(fieldProp == null){
+            return fallback;
+        }
+        return ((ArrayList<T>) fieldProp).get(0);
+    }
+    
     public static String[] getFields(){
         return new String[]{
                 "id",
